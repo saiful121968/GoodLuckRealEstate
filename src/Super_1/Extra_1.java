cb072c95c1d9e34dedb08c8f61d0fe0c29312498
saiful <saiful121968@gmail.com>
2018-01-05 15:02:44 -0500

    File added

---
+++ src/Super_1/Extra_1.java
@@ -0,0 +1,20 @@
+package Super_1;
+
+public class Extra_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x, y;
+		boolean z ;
+		x = 15;
+		y = 10;
+
+		System.out.println("x>y  is " + (x+y));
+		z = y>x;
+		System.out.println("y>x is " + z);
+
+
+	}
+
+}
